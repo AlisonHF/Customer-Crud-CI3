783c721685f6bd91ebf1c849c8b871f87a8ae0d4
Alison <alisonfaria2013@gmail.com>
2025-10-07 21:37:02 -0300

    Adicionado API de busca de CNPJ e melhoria na API de CEP

--- application/views/form.php
+++ application/views/form.php
@@ -3,14 +3,14 @@
 <form method="POST" class="form-control bg-dark mt-3 mb-5" id="form" data-bs-theme="dark">
 
     <label class="form-label">Nome/Razão:</label>
-    <input class="form-control type="text" name="nome" value="<?= isset($cliente) ? $cliente->nome_razao : set_value('nome') ?>" maxlength="255">
+    <input class="form-control" type="text" id="nome" name="nome" value="<?= isset($cliente) ? $cliente->nome_razao : set_value('nome') ?>" maxlength="255">
     <div class="form-invalid">
         <?php echo form_error('nome') ?>
     </div>
 
     <label class="form-label">CPF/CNPJ:</label>
-    <input class="form-control type="text" name="cpf_cnpj" value="<?= isset($cliente) ? $cliente->cpf_cnpj : set_value('cpf_cnpj') ?>" maxlength="14">
-    <div class="form-invalid">
+    <input class="form-control" type="text" id="cpf_cnpj" name="cpf_cnpj" value="<?= isset($cliente) ? $cliente->cpf_cnpj : set_value('cpf_cnpj') ?>" onblur="searchCnpj()" maxlength="14">
+    <div class="form-invalid" id="div-error-cnpj">
         <?php echo form_error('cpf_cnpj') ?>
         <?php if(isset($data['error_cpf_cnpj'])){
             echo $data['error_cpf_cnpj'];
@@ -19,35 +19,34 @@
     </div>
 
     <label class="form-label">E-mail</label>
-    <input class="form-control type="email" name="email" value="<?= isset($cliente) ? $cliente->email : set_value('email') ?>" maxlength="255">
+    <input class="form-control" type="email" name="email" value="<?= isset($cliente) ? $cliente->email : set_value('email') ?>" maxlength="255">
         <div class="form-invalid">
         <?php echo form_error('email') ?>
     </div>
 
     <label class="form-label">Telefone</label>
-    <input class="form-control type="number" name="telefone" value="<?= isset($cliente) ? $cliente->telefone : set_value('telefone') ?>" maxlength="19">
+    <input class="form-control" type="text" id="telefone" name="telefone" value="<?= isset($cliente) ? $cliente->telefone : set_value('telefone') ?>" maxlength="19">
     <div class="form-invalid">
         <?php echo form_error('telefone') ?>
     </div>
 
     <label class="form-label">CEP</label>
-    <input class="form-control type="number" id="cep" name="cep" value="<?= isset($cliente) ? $cliente->cep : set_value('cep') ?>" onblur="buscarCep()" maxlength="8">
-    <div class="form-invalid" id="div-cep">
+    <input class="form-control" type="text" id="cep" name="cep" value="<?= isset($cliente) ? $cliente->cep : set_value('cep') ?>" onblur="searchCep()" maxlength="8">
+    <div class="form-invalid" id="div-error-cep">
         <?php echo form_error('cep') ?>
     </div>
 
     <label class="form-label">Endereço</label>
-    <input class="form-control type="text" id="endereco" name="endereco" value="<?= isset($cliente) ? $cliente->endereco : set_value('endereco') ?>" maxlength="255">
+    <input class="form-control" type="text" id="endereco" name="endereco" value="<?= isset($cliente) ? $cliente->endereco : set_value('endereco') ?>" maxlength="255">
     <div class="form-invalid">
         <?php echo form_error('endereco') ?>
     </div>
 
     <label class="form-label">Cidade</label>
-    <input class="form-control type="text" id="cidade" name="cidade" value="<?= isset($cliente) ? $cliente->cidade : set_value('cidade') ?>" maxlength="100">
+    <input class="form-control" type="text" id="cidade" name="cidade" value="<?= isset($cliente) ? $cliente->cidade : set_value('cidade') ?>" maxlength="100">
     <div class="form-invalid">
         <?php echo form_error('cidade') ?>
     </div>
-
 
     <label class="form-label">UF</label>
     <input class="form-control type="text" id="uf" name="uf" value="<?= isset($cliente) ? $cliente->uf : set_value('uf') ?>" maxlength="2">
@@ -59,7 +58,3 @@
 </form>
 
 <a href="/Cliente" class="mt-3 mb-3 btn btn-primary" id="link-list">Voltar a listagem</a>
-
-<script>
-    //buscaCnpj('17467171000128');
-</script>
--- assets/scripts.js
+++ assets/scripts.js
@@ -1,72 +1,101 @@
-async function buscarCep()
-{
-    // Variaveis
-    let cepInput = $('#cep').val();
-    cepInput = cepInput.trim();
+function searchCep() {
+	// Variaveis
+	let cepInput = $("#cep").val();
+	cepInput = cepInput.trim();
 
-    let enderecoInput = $('#endereco');
-    let cidadeInput = $('#cidade');
-    let ufInput = $('#uf');
+	let enderecoInput = $("#endereco");
+	let cidadeInput = $("#cidade");
+	let ufInput = $("#uf");
 
-    const resultadoDiv = $('#div-cep');
+	const resultadoDiv = $("#div-error-cep");
+	resultadoDiv.html("");
 
-    let regex = /^\d+$/; // Somente numeros
+	let regex = /^\d+$/; // Somente numeros
 
-    // Verifica se o campo digitado é valido
-    if (!cepInput) {
-        resultadoDiv.html("Digite um CEP!");
-        return;
-    }
+	// Verifica se o campo digitado é valido
+	if (!cepInput) {
+		resultadoDiv.html("Digite um CEP!");
+		return null;
+	} else if (cepInput.length != 8) {
+		resultadoDiv.html("O CEP deve conter 8 números!");
+		return null;
+	} else if (!regex.test(cepInput)) {
+		resultadoDiv.html("Digite somente os números!");
+		return null;
+	}
 
-    else if (cepInput.length != 8)
-    {
-        resultadoDiv.html("O CEP deve conter 8 números!");
-        return;
-    }
+	// Realiza a requisição
+	fetch(`https://viacep.com.br/ws/${cepInput}/json/`)
+		.then((response) => {
+			if (response.status === 404 || response.status === 400) {
+				resultadoDiv.html("CEP não encontrado!");
 
-    else if (!regex.test(cepInput))
-    {
-        resultadoDiv.html("Digite somente os números!");
-        return;
-    }
+				return null;
+			}
 
-    // Realiza a requisição
-    try
-    {
-        const response = await fetch(`https://viacep.com.br/ws/${cepInput}/json/`);
-        const data = await response.json();
+			return response.json();
+		})
+		.then((data) => {
+			if (data.erro) {
+				resultadoDiv.html("CEP não encontrado!");
 
-        if (!data.erro) 
-        {
-            let endereco = data.logradouro + ', ' + data.bairro;
+				return null;
+			}
 
-            enderecoInput.val(endereco);
-            cidadeInput.val(data.localidade);
-            ufInput.val(data.uf);
-        }
+			let endereco = data.logradouro + ", " + data.bairro;
 
-        else
-        {
-            resultadoDiv.html("CEP não encontrado no banco de dados!", error);
-            return;
-        }
-       
-    }
-    
-    catch (error) // Caso for retornado um erro pela requisicao
-    {
-        resultadoDiv.html("Erro ao buscar o CEP, digite os dados manualmente...", error);
-        return;
-    }
-
+			enderecoInput.val(endereco);
+			cidadeInput.val(data.localidade);
+			ufInput.val(data.uf);
+		})
+		.catch((error) => {
+			resultadoDiv.html(`A requisição falhou! (status: ${error})`);
+		});
 }
 
+function searchCnpj() {
+	let cnpjInput = $("#cpf_cnpj").val();
+	cnpjInput.trim();
 
-/*
-async function buscaCnpj(cpf_cnpj)
-{
-    const response = await fetch('https://brasilapi.com.br/api/cnpj/v1/' + cpf_cnpj);
-    const data = response.json();
-    console.log(data);
+	let resultDiv = $("#div-error-cnpj");
+	resultDiv.html("");
+
+	if (cnpjInput.length != 14) {
+		return null;
+	}
+
+	let razaoSocialInput = $("#nome");
+	let telefoneInput = $("#telefone");
+
+	fetch("https://brasilapi.com.br/api/cnpj/v1/" + cnpjInput)
+		.then((response) => {
+			if (!response.ok) {
+				// Caso for retornado um erro pela requisicao
+				if (response.status === 404 || response.status === 400) {
+					resultDiv.html("CNPJ não encontrado!");
+				} else {
+					resultDiv.html(
+						"Não foi possível buscar o CNPJ, digite os dados manualmente!"
+					);
+				}
+
+				return null;
+			}
+
+			return response.json(); // Retorna os dados caso o CNPJ for válido
+		})
+		.then((data) => {
+			if (!data) {
+				return null;
+			}
+
+			// Caso os dados sejam válidos
+
+			razaoSocialInput.val(data.razao_social);
+			telefoneInput.val(data.ddd_telefone_1);
+		})
+		.catch((error) => {
+			// Caso for encontrado algum erro
+			console.error("Erro na requisição, status: ", error);
+		});
 }
-*/
