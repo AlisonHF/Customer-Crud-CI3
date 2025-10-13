a38965c0a39c4d5551f652902fb7c4d402425ae8
Alison <alisonfaria2013@gmail.com>
2025-10-13 19:36:07 -0300

    Ajustado helper de listagem de telefone

--- application/helpers/format_telefone_helper.php
+++ application/helpers/format_telefone_helper.php
@@ -2,15 +2,20 @@
 
 function format_telefone($telefone)
 {
-    if (empty($telefone))
-    {
+    if (empty($telefone)){
         return '';
     }
 
     $formatted_telefone = substr_replace($telefone, '(', 0, 0);
     $formatted_telefone = substr_replace($formatted_telefone, ')', 3, 0);
     $formatted_telefone = substr_replace($formatted_telefone, ' ', 4, 0);
-    $formatted_telefone = substr_replace($formatted_telefone, '-', 10, 0);
+
+    if (strlen($formatted_telefone) > 13){
+        $formatted_telefone = substr_replace($formatted_telefone, '-', 10, 0);
+        return $formatted_telefone;
+    }
+
+    $formatted_telefone = substr_replace($formatted_telefone, '-', 9, 0);
 
     return $formatted_telefone;
 }
